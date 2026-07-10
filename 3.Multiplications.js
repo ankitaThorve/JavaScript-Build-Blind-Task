f2f3979e26b7cebcf51b4af01ed129fb314595b4
Ankita <ankietathorve@gmail.com>
2026-07-10 18:59:41 +0530

    Multiplication using while loop

--- 3.Multiplications.js
+++ 3.Multiplications.js
@@ -4,3 +4,11 @@ for (let i = 1; i <= 10; i++) {
   console.log(`${num} x ${i} = ${num * i}`);
 }
 
+// using while loop
+const num1 = 6;
+let i = 1;
+console.log("\n");
+while (i <= 10) {
+  console.log(`${num1} x ${i} = ${num1 * i}`);
+  i++;
+}
