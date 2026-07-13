06dc6d5f955292e444f2ebd0d7121bf8d69e69d0
Ankita <ankietathorve@gmail.com>
2026-07-13 23:44:16 +0530

    FizzBuzz function - returning array

--- 4.FizzBuzz.js
+++ 4.FizzBuzz.js
@@ -14,3 +14,22 @@ function runFizzBuzz(limit) {
 }
 
 console.log(runFizzBuzz(15)); //argument passedm with limit
+
+// Returning an array ----
+function getFizzBuzzArray(n) {
+  const result = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
+    } else if (i % 5 === 0) {
+      result.push("Buzz");
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
+}
+
+console.log(getFizzBuzzArray(15));
