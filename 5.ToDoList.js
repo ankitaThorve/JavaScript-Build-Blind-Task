cdf90c7a1bc37826a89550a8f5e7825e87ea702e
Ankita <ankietathorve@gmail.com>
2026-07-15 21:28:09 +0530

    View Task using map() method

--- 5.ToDoList.js
+++ 5.ToDoList.js
@@ -17,3 +17,21 @@ function addTask(taskName) {
   todos.push(newTask);
   console.log(`✅ Added: ${taskName}`);
 }
+
+// View Tasks (map)
+function viewTask() {
+  if (todos.length === 0) {
+    console.log("No tasks available");
+    return;
+  }
+
+  console.log(`\n----------To Do List----------`);
+
+  const taskList = todos.map((item) => {
+    const status = item.completed ? "✅" : "❌";
+    return `${item.id}. ${status} ${item.task}`;
+  });
+
+  taskList.forEach((task) => console.log(task));
+  console.log("----------\n");
+}
