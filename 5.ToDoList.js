7b00d9436d64d41408e875fc08d283cd5c441dc5
Ankita <ankietathorve@gmail.com>
2026-07-15 21:17:17 +0530

    Add task using push() method

--- 5.ToDoList.js
+++ 5.ToDoList.js
@@ -1,2 +1,19 @@
 // Storage
-let todos=[]
+let todos = [];
+
+// Add task (push)
+function addTask(taskName) {
+  if (!taskName || typeof taskName !== "String") {
+    console.log(`❌ Please enter a valid task.`);
+    return;
+  }
+
+  const newTask = {
+    id: todos.length + 1,
+    task: taskName,
+    completed: false,
+  };
+
+  todos.push(newTask);
+  console.log(`✅ Added: ${taskName}`);
+}
